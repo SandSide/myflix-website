a31e00032671041748186e759458b22c66f46339
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-27 23:02:51 +0000

    feat: create controller for videos

---
+++ src/myflix-website/Controllers/VideoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myflix_website.Services;
+
+namespace myflix_website.Controllers
+{
+    public class VideoController : Controller
+    {
+        private readonly IVideoService _videoService;
+
+        public VideoController(IVideoService videoService) { 
+            _videoService = videoService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var videos = await _videoService.GetVideoAsync();
+            return View(videos);
+        }
+    }
+}
