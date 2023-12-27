2fba5fa1e9ddbdd95ca78a49efa5e4b520f90f98
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-27 22:20:57 +0000

    feat: create service to get videos

---
+++ src/myflix-website/Services/IVideoService.cs
@@ -0,0 +1,9 @@
+﻿using myflix_website.Models;
+
+namespace myflix_website.Services
+{
+    public interface IVideoService
+    {
+        Task<List<Video>> GetVideoAsync();
+    }
+}
---
+++ src/myflix-website/Services/VideoService.cs
@@ -0,0 +1,27 @@
+﻿using myflix_website.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace myflix_website.Services
+{
+    public class VideoService : IVideoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public VideoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Video>> GetVideoAsync()
+        {
+            var response = await _httpClient.GetAsync("http://http://35.184.60.220/api/Videos");
+            response.EnsureSuccessStatusCode();
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var videos = JsonSerializer.Deserialize<List<Video>>(jsonString);
+
+            return videos;
+        }
+    }
+}
