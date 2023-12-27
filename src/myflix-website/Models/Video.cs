7b45133aa0c77f93ee86cf4f38ac9079dbb9d292
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-27 22:01:46 +0000

    feat: create Video class based on api response JSON

---
+++ src/myflix-website/Models/Video.cs
@@ -0,0 +1,12 @@
+﻿namespace myflix_website.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int Length { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
