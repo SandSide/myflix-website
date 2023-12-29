668cb8f7b2c4863446d8e2a68062e9facff88a68
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-29 01:11:58 +0000

    feat: add 'ViewVideo' action method to 'VideoController'

--- src/myflix-website/Controllers/VideoController.cs
+++ src/myflix-website/Controllers/VideoController.cs
@@ -16,5 +16,16 @@ namespace myflix_website.Controllers
             var videos = await _videoService.GetVideoCatalogueAsync();
             return View(videos);
         }
+
+        [HttpGet("video/{filename}")]
+        public async Task<IActionResult> ViewVideo(string filename)
+        {
+            byte[] videoData = await _videoService.GetVideoFromUrlAsync(filename);
+           
+            string videoBase64 = Convert.ToBase64String(videoData);
+            ViewBag.VideoBase64 = videoBase64;
+
+            return View();
+        }
     }
 }
